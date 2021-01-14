a29e85fb5e21bd4d56503d363f2a0748996308f6
Gabriel Dumitru <martesciuc.gabriel@gmail.com>
2021-01-14 19:59:12 +0200

    Added get env

--- common/lib/xmodule/xmodule/video_module/video_module.py
+++ common/lib/xmodule/xmodule/video_module/video_module.py
@@ -793,7 +793,7 @@ class VideoBlock(
         """
         if jwplayer_media_id:
             expires = math.ceil((time.time() + 3600) / 300) * 300
-            API_SECRET = os.environ["JWPLATFORM_API_SECRET"]
+            API_SECRET = os.getenv("JWPLATFORM_API_SECRET")
             host="https://content.jwplatform.com/"
             path = u'videos/{0}.mp4'.format(jwplayer_media_id)
 
